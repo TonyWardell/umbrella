bcc35fa98b8e6395e7ec2c0bd08367bb28001613
TonyWardell <tonyandamie@yahoo.co.uk>
2018-04-07 09:16:40 +0100

    A list based QuickSort.

---
+++ src/main/java/uk/wardell/tony/quicksort/QuickSort.java
@@ -0,0 +1,49 @@
+package uk.wardell.tony.quicksort;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        var input = Arrays.asList("sofa", "chair", "light", "amp", "piano", "television", "table");
+        Comparator<String> comparator = Comparator.naturalOrder();
+        QuickSort quickSort = new QuickSort();
+        List<String> output = quickSort.sort(input, comparator);
+        Stream.of(output).forEach(e -> System.out.println(e));
+    }
+
+    public List<String> sort(List<String> input, Comparator<String> comparator){
+        Objects.requireNonNull(input);
+        if(input.size() <= 1){
+            return input;
+        }
+
+        int pivotPosition = positionBetweenLeftAndRight(input);
+        String pivotValue = input.get(pivotPosition);
+
+        List<String> lower = input.stream()
+                .filter(e -> comparator.compare(e, pivotValue) < 0)
+                .collect(Collectors.toList());
+
+        List<String> upper = input.stream()
+                .filter(e -> comparator.compare(e, pivotValue) >= 0)
+                .collect(Collectors.toList());
+
+        List<String> sortedLower = sort(lower, comparator);
+        List<String> sortedUpper = sort(upper, comparator);
+
+        var result = new ArrayList<String>();
+        result.addAll(sortedLower);
+        result.addAll(sortedUpper);
+        return result;
+    }
+
+    static Random rgen = new Random();
+
+    // produce a random number in the left to right range
+    static int positionBetweenLeftAndRight(List<String> input){
+        return Math.abs(rgen.nextInt()) % (input.size());
+    }
+}
