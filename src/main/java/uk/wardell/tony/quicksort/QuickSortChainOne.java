0c6bafdef0dbb5d50fd384ad96947e291ae950ab
TonyWardell <tonyandamie@yahoo.co.uk>
2018-04-08 07:48:33 +0100

    Change to use Strings and related main method. Break up code.

--- src/main/java/uk/wardell/tony/quicksort/QuickSortChainOne.java
+++ src/main/java/uk/wardell/tony/quicksort/QuickSortChainOne.java
@@ -13,28 +13,35 @@ import java.util.stream.Stream;
  */
 
 public class QuickSortChainOne {
-    private static Function<Integer, Predicate<Integer>> smallerThan = x -> y -> y < x;
+    private static Function<String, Predicate<String>> smallerThan = x -> y -> y.compareTo(x) < 0;
 
-    public static List<Integer> qsort(List<Integer> l) {
+    static Function<List<String>, List<String>> lowerStream = ll -> ll.stream()
+            .skip(1)
+            .filter(smallerThan.apply(ll.get(0)))
+            .collect(Collectors.toList());
+
+    static Function<List<String>, List<String>> upperStream = lu -> lu.stream()
+            .skip(1)
+            .filter(smallerThan.apply(lu.get(0)).negate())
+            .collect(Collectors.toList());
+
+    public static List<String> qsort(List<String> l) {
         if (l.isEmpty()) return new ArrayList<>();
 
-        return Stream.concat(Stream.concat(
-                qsort(l.stream()
-                        .skip(1)
-                        .filter(smallerThan.apply(l.get(0)))
-                        .collect(Collectors.toList()))
-                        .stream(),
-                Stream.of(l.get(0))),
-                qsort(l.stream()
-                        .skip(1)
-                        .filter(smallerThan.apply(l.get(0)).negate())
-                        .collect(Collectors.toList()))
-                        .stream())
-                .collect(Collectors.toList());
+        return Stream.concat(
+                                Stream.concat(qsort(lowerStream.apply(l)).stream(), Stream.of(l.get(0))),
+                                qsort(upperStream.apply(l)).stream()
+                            ).collect(Collectors.toList());
     }
 
+//    public static void main(String[] args) {
+//        List<Integer> l = Arrays.asList(5, 6, 7, 23, 4, 5645, 6, 1223, 44453, 60182, 2836, 23993, 1);
+//        System.out.println(qsort(l));
+//    }
+
     public static void main(String[] args) {
-        List<Integer> l = Arrays.asList(5, 6, 7, 23, 4, 5645, 6, 1223, 44453, 60182, 2836, 23993, 1);
-        System.out.println(qsort(l));
+        var input = Arrays.asList("sofa", "chair", "light", "amp", "piano", "television", "table");
+        List<String> output = QuickSortChainOne.qsort(input);
+        Stream.of(output).forEach(System.out::println);
     }
 }
